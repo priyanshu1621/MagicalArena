d4df07ff9b7dd8fcf965a2283586cebe888d8c27
Priyanshu Gaur <gaur.priyanshu169@gmail.com>
2024-11-12 16:58:16 +0530

    Write Basic function of  1. Dice roll function 2. Taking string input by user 3. intput integer from user

---
+++ GameFunction.js
@@ -0,0 +1,46 @@
+const readline = require("readline");
+
+// dice roll function
+function rollDice() {
+  let min = 1,
+    max = 6;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// take string as input from user
+function inputStringFromUser(prompt) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  return new Promise((resolve) => {
+    rl.question(prompt, (inputString) => {
+      rl.close();
+      resolve(inputString);
+    });
+  });
+}
+
+// take integer as input from user
+function inputIntegerFromUser(promptMessage) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(promptMessage, (inputString) => {
+      rl.close();
+      const userInput = parseInt(inputString.trim(), 10);
+      if (!isNaN(userInput)) {
+        resolve(userInput);
+      } else {
+        console.log("Invalid input. Please enter a valid integer.");
+        resolve(inputIntegerFromUser(promptMessage));
+      }
+    });
+  });
+}
+
+module.exports = { rollDice, inputStringFromUser, inputIntegerFromUser };
