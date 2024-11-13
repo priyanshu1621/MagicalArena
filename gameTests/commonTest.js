d25026a9d0346ee91ef55de317f9bfb614113ad3
Priyanshu Gaur <gaur.priyanshu169@gmail.com>
2024-11-13 09:45:15 +0530

    write common test for dice roll and the range is btw 1-6

---
+++ gameTests/commonTest.js
@@ -0,0 +1,17 @@
+const { rollDice } = require("../utils/GameFunction");
+
+describe("rollDice function", () => {
+  
+  // dice roll output should be in range of 1 -> 6
+  test("returns a number between 1 and 6", () => {
+    const result = rollDice();
+    expect(result).toBeGreaterThanOrEqual(1);
+    expect(result).toBeLessThanOrEqual(6);
+  });
+
+  // make sure that dice roll output should be an integer
+  test("returns an integer", () => {
+    const result = rollDice();
+    expect(Number.isInteger(result)).toBe(true);
+  });
+});
